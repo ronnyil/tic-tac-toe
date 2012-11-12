3033098e095d27e496cf2aa4f298abaa55e810e9
Raanan <r.n.lipkin@gmail.com>
2012-11-12 13:45:23 +0200

    Added new tests. Supported more exceptions.

---
+++ XOGame/XOInvalidMoveException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOGame
+{
+    public enum XOInvalidMoveExceptionReasons
+    {
+        SamePlayerTwice,
+        AlreadySetPlace
+    }
+    public class XOInvalidMoveException : Exception
+    {
+        public XOInvalidMoveException(string message,XOInvalidMoveExceptionReasons reason) : base(message)
+        {
+
+        }
+        public XOInvalidMoveExceptionReasons Reason { get; set; }
+    }
+}
